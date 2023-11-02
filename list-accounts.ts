856c8a0a8072b1dcc39aaa94f7a1268d29d1f48d
Gabriel <961534+GabrielGil@users.noreply.github.com>
2023-11-02 12:54:13 +0100

    feat: Create list-accounts.ts example

---
+++ list-accounts.ts
@@ -0,0 +1,16 @@
+import { Tradr } from '@tradrapi/trading-sdk';
+
+/** Development key */
+const API_KEY = 'bHxKy1w3B+kvbbN4KymBp9ZE6X4z8hFP3LwNVUI9tpM=';
+
+const tradrApi = Tradr.make({
+  exchange: {
+    serviceUrl: 'https://dev.tradrapi.com',
+    socketUrl: 'wss://dev.tradrapi.com',
+  },
+  auth: { apiKey: API_KEY },
+  debug: true,
+});
+
+/* Output the accounts */
+tradrApi.accounts.get(100039504).then(console.log);
