5e648b2247118ea8a0350ee5065cff1798ece76f
Nima Arefi <nimaarefi@outlook.com>
2024-04-03 17:09:11 +0330

    feat: Add examples for deno run time
    examples now ask for api key and accountID once user run the script so the script work as expected

---
+++ deno/account-balance.ts
@@ -0,0 +1,20 @@
+// retrieve trading account balance, equity, and margin information
+
+import { Tradr, Config } from "@tradrapi/trading-sdk";
+
+const apiKey = prompt("Enter your API key:");
+
+// the id of the account to retrieve
+const accountId = prompt("Enter the account ID to retrieve:");
+
+// configure the SDK
+const sdkConfig: Config = {
+  auth: { apiKey },
+  debug: true,
+};
+
+// create an instance of the SDK
+const tradrApi = Tradr.make(sdkConfig);
+
+// retrieve the account
+tradrApi.balances.get(accountId).then((data) => console.log(data));
